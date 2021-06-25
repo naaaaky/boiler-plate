2862068a1efe21cd8f30e3eef4c9db5c7adcf5fc
Nakyeong Ha <nkha@atozsoft.kr>
2021-06-25 17:34:46 +0900

    bcrypt

--- models/User.js
+++ models/User.js
@@ -35,11 +35,26 @@ const userSchema = mongoose.Schema({
 
 //user collection에 저장하기 전에 pre라는 미들웨어(몽구스에서 제공됨) 함수를 이용하여
 //save 메소드 수행 시 처리할 내용을 작성
-userSchema.pre('save', (next) => {
-    //비밀번호를 암호화하는 작업
-    // bcrypt
+//arrow function을 사용하지 않는 이유는 this를 바인딩할 수 없기 때문에 -> arrow function을 사용하면 user객체의 정보를 가져올 수 없다.
+userSchema.pre('save', function (next) {
+    var user = this;
 
-    next();
+    //isModified는 mongoose 내장함수
+    if (user.isModified('password')) {
+        //비밀번호를 암호화하는 작업
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+            else {
+                bcrypt.hash(user.password, salt, function (err, hash) {
+                    if (err) return next(err);
+                    else {
+                        user.password = hash;   //password를 hash된 password로 변경해준다.
+                        next();
+                    }
+                });
+            }
+        });
+    }
 });
 
 const User = mongoose.model('User', userSchema);
