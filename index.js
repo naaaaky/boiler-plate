acc04a1e4514b79174099ca1b308555cd850fa53
Nakyeong Ha <nkha@atozsoft.kr>
2021-06-28 01:03:12 +0900

    generate token w/ jsonwebtoken

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
 const { User } = require('./models/User');
 
-app.use(bodyParser.urlencoded({extended: true}));   //application/x-www-form-urlencoded를 분석하여 가져오기 위한 설정
+// 미들웨어 함수를 로딩
+app.use(bodyParser.urlencoded({ extended: true })); //application/x-www-form-urlencoded를 분석하여 가져오기 위한 설정
 app.use(bodyParser.json()); //application/json을 분석하여 가져오기 위한 설정
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose
-  .connect(
-    config.mongoURI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log('MongoDB connected success'))
   .catch((err) => console.log(err));
 
@@ -36,35 +36,44 @@ app.post('/register', (req, res) => {
 
   //user 모델에 저장하기 위해 save라는 mongodb의 내장함수를 이용
   user.save((err, userInfo) => {
-      if(err) return res.json({success: false, err});
-      return res.status(200).json({success: true});
-  });  
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.post('/login', (req, res) => {
-    // 1. 요청된 이메일이 db에 있는지 쿼리
-    User.findOne({email: req.body.email}, (err, user) => {
-        if(!user) { 
-            // 일치하는 사용자가 없으면
-            return res.json({
-                loginSuccess: 'false',
-                message: '일치하는 사용자가 없습니다.'
-            });
+  // 1. 요청된 이메일이 db에 있는지 쿼리
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      // 일치하는 사용자가 없으면
+      return res.json({
+        loginSuccess: 'false',
+        message: '일치하는 사용자가 없습니다.',
+      });
+    } else {
+      // 2. 요청된 이메일이 db에 있다면 비밀번호가 일치하는지 확인.
+      // User 스키마를 정의한 User.js파일(model)에 comparePassword 메소드를 만든다.
+      // comparePassword 메소드의 콜백함수(인자이름 cb)가 두번째 인자로 오게 된다.
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch) {
+          return res.json({ loginSuccess: 'false', message: '비밀번호가 일치하지 않습니다.' });
         } else {
-            // 2. 요청된 이메일이 db에 있다면 비밀번호가 일치하는지 확인.
-            // User 스키마를 정의한 User.js파일에 comparePassword 함수를 만든다.
-            user.comparePassword(req.body.password, (err, isMatch) => {
-                if(!isMatch) {
-                    return res.json({loginSuccess: 'false', message: '비밀번호가 일치하지 않습니다.'});
-                } else {
-                    // 3. 비밀번호가 일치하면 토큰 생성
-                    user.generateToken((err, user) => {
-                        
-                    });
-                }
-            });
+          // 3. 비밀번호가 일치하면 토큰 생성 (jsonwebtoken 라이브러리)
+          user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            //400번 에러가 뜨면 에러메시지 반환
+            else {
+              // storage/cookie에 토큰을 저장한다
+              res.cookie('x_auth', user.token).status(200).json({
+                loginSuccess: 'true',
+                userId: user._id,
+              });
+            }
+          });
         }
-    });
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -37,36 +38,54 @@ const userSchema = mongoose.Schema({
 //save 메소드 수행 시 처리할 내용을 작성
 //arrow function을 사용하지 않는 이유는 this를 바인딩할 수 없기 때문에 -> arrow function을 사용하면 user객체의 정보를 가져올 수 없다.
 userSchema.pre('save', function (next) {
-    var user = this;
+  var user = this;
 
-    //isModified는 mongoose 내장함수
-    if (user.isModified('password')) {  //비밀번호를 바꿀때
-        //비밀번호를 암호화하는 작업
-        bcrypt.genSalt(saltRounds, function (err, salt) {
-            if (err) return next(err);
-            else {
-                bcrypt.hash(user.password, salt, function (err, hash) {
-                    if (err) return next(err);
-                    else {
-                        user.password = hash;   //password를 hash된 password로 변경해준다.
-                        next();
-                    }
-                });
-            }
+  //isModified는 mongoose 내장함수
+  if (user.isModified('password')) {
+    //비밀번호를 바꿀때
+    //비밀번호를 암호화하는 작업
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      else {
+        bcrypt.hash(user.password, salt, function (err, hash) {
+          if (err) return next(err);
+          else {
+            user.password = hash; //password를 hash된 password로 변경해준다.
+            next();
+          }
         });
-    } else {
-        next();
-    }
+      }
+    });
+  } else {
+    //비밀번호 이외의 정보를 변경할때
+    next();
+  }
 });
 
-// 사용자가 입력한 패스워드(plainPassword)와 암호화된 패스워드가 일치하는지 비교하는 함수
+// 사용자가 입력한 패스워드(plainPassword)와 암호화된 패스워드가 일치하는지 비교하는 메소드
 // 이미 암호화된 패스워드는 복호화할수 없으므로 plainPassword를 암호화하여 비교
-userSchema.methods.comparePassword = function(plainPassword, cb) {
-    // plainPassword 인자는 사용자가 입력한 패스워드
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
-        if(err) return cb((err));
-        cb(null, isMatch);  //null은 에러가 없다, isMatch는 일치여부 bool type
-    });
+// 아래에서 정의한 comparePassword라는 인스턴스 메소드에서 this는 데이터 인스턴스를 가르킨다.
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword 인자는 사용자가 입력한 패스워드
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch); //null은 에러가 없다, isMatch는 일치여부 bool type
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  // 객체 안에서 선언된 함수의 this는 User 객체가 아닌 window 객체를 바라보고 있기 때문에
+  // this를 user 변수 안에 담아준다. (es6 이전에만 해당됨)
+  var user = this;
+
+  // jsonwebtoken을 이용하여 token 생성
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
 };
 
 const User = mongoose.model('User', userSchema);
